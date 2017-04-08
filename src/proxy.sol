fd8b56ba03da7fd3255291ec23e10c752cfe8d9b
NiklasKunkel <Niklas@theKunkels.com>
2017-04-08 12:41:00 -0700

    Fixed issue with contract creation

--- src/proxy.sol
+++ src/proxy.sol
@@ -20,27 +20,26 @@ import "ds-auth/auth.sol";
 import "Ds-note/note.sol";
 
 contract DSProxy is DSAuth, DSNote {
-	//fallback function
+
 	function execute(bytes _code, bytes _data)
 		auth
 		note
 		payable
 		returns (bytes32 response)
-	{	
+	{
 		uint256 codeLength = _code.length;
 		uint256 dataLength = _data.length;
-		
+
 		assembly {
-			let pMem := mload(0x40)                     //load free memory pointer
-			calldatacopy(pMem, _code, codeLength)       //copy contract code from calldata to memory
-			let target := create(0, pMem, codeLength)   //deploy contract
-			jumpi(0x02, iszero(target))                 //verify address of deployed contract
-			calldatacopy(pMem, _data, dataLength)       //copy request data from calldata to memory
-			let succeeded := delegatecall(gas, target, pMem, dataLength, pMem, 32) //call deployed contract
-			jumpi(0x02, iszero(succeeded))              //throw if delegatecall failed
-			response := mload(pMem)                     //set delegatecall output to response
+			let target := create(0, add(_code, 0x20), mload(_code))	//deploy contract
+			jumpi(0x02, iszero(target))                 			//verify address of deployed contract
+			//calldatacopy(pMem, _data, dataLength)       			//copy request data from calldata to memory
+			let pMem := mload(0x40)                     			//load free memory pointer
+			let succeeded := delegatecall(gas, target, add(_data, 0x20), mload(_data), pMem, 32) //call deployed contract
+			jumpi(0x02, iszero(succeeded))              			//throw if delegatecall failed
+			response := mload(pMem)                     			//set delegatecall output to response
 		}
-		return response;
+		return response;		
 	}
 }
 

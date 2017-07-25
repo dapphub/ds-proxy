a3eb5c8d32909edb5ceb891a9952f31f6eef5c99
NiklasKunkel <Niklas@theKunkels.com>
2017-07-25 13:22:24 -0700

    updated pragma to 0.4.13

--- src/proxy.sol
+++ src/proxy.sol
@@ -13,7 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-pragma solidity ^0.4.9;
+pragma solidity ^0.4.13;
 
 import "ds-auth/auth.sol";
 import "ds-note/note.sol";
--- src/test.sol
+++ src/test.sol
@@ -13,7 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-pragma solidity ^0.4.9;
+pragma solidity ^0.4.13;
 
 import "ds-test/test.sol";
 import "./proxy.sol";

351eaaa8ff68df2a0d46dfdcc3b5cd62f77d6d9e
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-10-12 07:56:57 -0300

    Add execute function using target address

--- src/proxy.sol
+++ src/proxy.sol
@@ -36,20 +36,30 @@ contract DSProxy is DSAuth, DSNote {
     // use the proxy to execute calldata _data on contract _code
     function execute(bytes _code, bytes _data)
         public
-        auth
-        note
         payable
-        returns (bytes32 response)
+        returns (address target, bytes32 response)
     {
-        address target = cache.read(_code);
+        target = cache.read(_code);
         if (target == 0x0) {
             // deploy contract & store its address in cache
             target = cache.write(_code);
         }
 
+        response = execute(target, _data);
+    }
+
+    function execute(address _target, bytes _data)
+        public
+        auth
+        note
+        payable
+        returns (bytes32 response)
+    {
+        require(_target != 0x0);
+
         // call contract in current context
         assembly {
-            let succeeded := delegatecall(sub(gas, 5000), target, add(_data, 0x20), mload(_data), 0, 32)
+            let succeeded := delegatecall(sub(gas, 5000), _target, add(_data, 0x20), mload(_data), 0, 32)
             response := mload(0)      // load delegatecall output
             switch iszero(succeeded)
             case 1 {
@@ -70,11 +80,6 @@ contract DSProxy is DSAuth, DSNote {
         if (_cacheAddr == 0x0) revert();   // invalid cache address
         cache = DSProxyCache(_cacheAddr);  // overwrite cache
         return true;
-    }
-
-    //get current cache
-    function getCache() public view returns (address) {
-        return cache;
     }
 }
 
